5450f344021c2eedbb638f2721e5efc3f26dbe83
beckyphan <beckyphan@beckys-macbook.lan>
2019-09-07 14:08:03 -0400

    updated welcome and app controler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,26 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "r-s-v-p"
   end
 
   get "/" do
     erb :welcome
+  end
+
+  helpers do
+    def current_user
+      @current_user ||=User.find_by(id: session[:user_id])
+    end
+
+    def logged_in?
+      current_user
+    end
+
+    def authorized_to_edit?(guest)
+      current_user == guest.user 
+    end
   end
 
 end
