a9b91755f4402b249983ceeb7ec45c8c00f147e3
Becky Phan <beckyphan@live.com>
2019-09-07 15:08:14 -0400

    modified user has_secure_password, signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    erb :'users/new'
+  end
+
   get '/login' do
+    erb :'users/login'
   end
 
   post '/login' do
-    
+    binding.pry
+    @user = User.create(params[:user])
+
+    redirect "/#{@user.username.slug}"
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   has_many :guests
+  has_secure_password
   validates :first_name, :last_name, :username, :email, :password, presence: true
+  validates :username, :email, uniqueness: true
+
+  extend Concerns::ClassMethods
+  include Concerns::InstanceMethods
 end
